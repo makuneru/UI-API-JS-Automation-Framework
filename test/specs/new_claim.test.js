7dde757af666ac7aa767fd565c0bdb2e3828ea88
zurctin <cruzmachris@gmail.com>
2024-10-04 11:21:20 +0800

    Updated new claim test

--- test/specs/new_claim.test.js
+++ test/specs/new_claim.test.js
@@ -1,7 +1,7 @@
 const LoginPage = require("../pageobjects/login.page");
 const SecurePage = require("../pageobjects/secure.page");
 
-describe("My Login application", () => {
+describe("My claim test application", () => {
   it("should login with valid credentials", async () => {
     await LoginPage.open();
 
